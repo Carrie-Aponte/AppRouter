c2aba214518f2eb20a841e8e368053a74b200f82
carrie <hall.carrie98@gmail.com>
2025-02-18 14:47:45 -0600

    Adding image, adding new route with dashboard

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Oswald, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const oswald = Oswald({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({ subsets: ["latin"], weight: ["400", "700"] });
