df0c263c516ebe5004cd184e3ed5358301387d28
Carrie Aponte <hall.carrie98@gmail.com>
2025-03-03 22:01:44 -0600

    implementing pagination!

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -16,6 +16,7 @@ export default function Search({ placeholder = "Search..." }: SearchProps) {
   const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
 
     if (term) {
       params.set("query", term);
